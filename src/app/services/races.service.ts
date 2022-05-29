08a66aa1c8f49de22ab5c45db4d4a64332d5c5ea
lrocamonde <lrocamonde@uoc.edu>
2022-05-29 19:21:48 +0200

    Cambio url

--- src/app/services/races.service.ts
+++ src/app/services/races.service.ts
@@ -10,7 +10,7 @@ export class RacesService {
   constructor(private http: HttpClient) { }
 
   getAllSeasons(): Observable<any> {
-    return this.http.get<any>('https://ergast.com/api/f1/seasons.json?limit=200');
+    return this.http.get<any>('https://ergast.com/api/f1/seasons.json?limit=100');
   }
 
   getSeason(id: string): Observable<any> {
